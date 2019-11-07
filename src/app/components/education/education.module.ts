ab4039238f90a1e844d2c0a4e576e85ee526982c
Andrii Lysiuk <avl.ms@outlook.com>
2019-11-07 17:27:20 +0200

    12

--- src/app/components/education/education.module.ts
+++ src/app/components/education/education.module.ts
@@ -1,5 +1,5 @@
 import { CoursesService } from './courses.service';
-import { EducationRoutingModule } from './education.routing.module';
+// import { EducationRoutingModule } from './education.routing.module';
 import { CoursesDetailsComponent } from './courses-details/courses-details.component';
 import { CoursesComponent } from './courses/courses.component';
 import { EducationComponent } from './education.component';
